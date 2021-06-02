2bfba1f868cca8b16e15f0b025fb9cca4dbe47cb
hankavtech <47284630+hankavtech@users.noreply.github.com>
2021-06-02 15:46:22 -0400

    Create Weather.sol

---
+++ Weather.sol
@@ -0,0 +1,14 @@
+
+pragma solidity ^0.5.6;
+
+contract Weather {
+    string public condition;
+
+    constructor() public {
+        condition = "";
+    }
+
+    function setCondition(string memory _condition) public {
+        condition = _condition;
+    }
+}
